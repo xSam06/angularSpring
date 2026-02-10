31d9eec822b187f6e5eedd044c89ae2f12cbd4c7
Sam <samybouamar06@gmail.com>
2026-02-10 16:08:37 +0100

    ajout d'un service pour projet

---
+++ frontend/src/app/service/project.service.ts
@@ -0,0 +1,24 @@
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Project} from '../model/project.model';
+import {Injectable} from '@angular/core';
+
+@Injectable({providedIn: 'root'})
+export class ProjectService{
+
+  private apiUrl = "http://localhost:8081/api/projects";
+
+  constructor(private http: HttpClient) {}
+
+  getProjects(): Observable<Project[]>{
+  return this.http.get<Project[]>(this.apiUrl);
+  }
+
+  addProject(project: Project): Observable<Project> {
+    return this.http.post<Project>
+    (`${this.apiUrl}/add`,project);
+  }
+
+
+
+}
